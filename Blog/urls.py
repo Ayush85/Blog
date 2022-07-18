885a21f924b9b2ec72842f990ed60cf95d50bdfe
Ayush85 <58679369+Ayush85@users.noreply.github.com>
2022-07-18 14:39:27 +0545

    Update urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from os import stat
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path , include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
