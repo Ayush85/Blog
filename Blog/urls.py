39e9e3860b2a0ffb21f285bceb378e500d9cfe04
Ayush85 <58679369+Ayush85@users.noreply.github.com>
2022-07-18 15:04:25 +0545

    Update urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('froala_editor/',include('froala_editor.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
