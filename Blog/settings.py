70056f52080e00eaa6824a674f7f6143252b2858
Ayush85 <58679369+Ayush85@users.noreply.github.com>
2022-07-18 13:33:44 +0545

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -31,7 +31,7 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = 'django-insecure-it&g9oad!ao_f=1fhyl^+0#&(lcb+%s8@y#lbs!5%t+5dludr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
