2fd63e12a18c689e7c330047520d59987971969f
Ayush85 <58679369+Ayush85@users.noreply.github.com>
2023-01-11 00:07:01 +0545

    Update info.py

--- Blog/info.py
+++ Blog/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = 'ayushshrestha396@gmail.com'  
-EMAIL_HOST_PASSWORD = 'hzszbstotggutocb'  
+EMAIL_HOST_USER = 'ayush85.stha@gmail.com'  
+EMAIL_HOST_PASSWORD = 'nukefzwuipvbqmks'  
 EMAIL_PORT = 587  
